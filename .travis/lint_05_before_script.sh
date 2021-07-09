c3ed161d2e4783ae425541e54c1a5f8f7073930b
SpectreSecurityCoin <development@SpectreSecurity.io>
2021-07-09 17:33:30 -0400

    Update

--- .travis/lint_05_before_script.sh
+++ .travis/lint_05_before_script.sh
@@ -5,6 +5,7 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 export LC_ALL=C
+git --recursive
 chmod +x /home/travis/build/Game-Frag/game-frag-coin/depends/config.guess
 chmod +x /home/travis/build/Game-Frag/game-frag-coin/depends/config.sub
 chmod -Rv 775 /home/travis/build/Game-Frag
