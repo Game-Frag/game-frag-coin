c381c3531b628cfe302141b099d65eb9de428e72
SpectreSecurityCoin <development@SpectreSecurity.io>
2021-07-09 15:08:10 -0400

    Update lint_06_script.sh

--- .travis/lint_06_script.sh
+++ .travis/lint_06_script.sh
@@ -6,8 +6,8 @@
 
 export LC_ALL=C
 
-chmod +x contrib/devtools/git-subtree-check.sh
-contrib/devtools/git-subtree-check.sh ./src/secp256k1
+chmod +x contrib/devtools/git-subtree-check.sh 
+contrib/devtools/git-subtree-check.sh src/secp256k1
 contrib/devtools/git-subtree-check.sh src/univalue
 contrib/devtools/git-subtree-check.sh src/leveldb
 contrib/devtools/git-subtree-check.sh src/crc32c
